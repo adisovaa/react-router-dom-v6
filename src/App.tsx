7336cb4644d8f815467cc375e0643daf8993385a
lorem <79251934+adisovaa@users.noreply.github.com>
2023-04-30 14:02:42 -0400

    04 - Links

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import './App.css';
 import {NavLink, Outlet, Route, Router, Routes, useParams} from "react-router-dom";
 
 const Profile = () => {
-    const params = useParams<'*'>()
-    const some = params['*']
+    const param = useParams<'*'>()
+    const some = param['*']
     console.log(some)
 
     return <div>Profile</div>
@@ -15,8 +15,16 @@ function App() {
         <div className="App">
             <NavLink to={'/'}>Main</NavLink> ---
             <NavLink to={'/login'}>Login</NavLink> ---
-            <NavLink to={'/profile'}>Profile</NavLink> ---
-            <NavLink to={'/profile/1'}>Profile</NavLink> ---
+
+            <NavLink to={'/profile'}
+                     style={(params) => {
+                         return {color: params.isActive ? 'lime' : 'black'}
+                     }}>Profile</NavLink> ---
+
+            <NavLink
+                to={'/profile/settings'}
+                className={({isActive}) => isActive ? 'act' : 'def'}
+            >Settings</NavLink> ---
 
 
             <Routes>
@@ -25,6 +33,13 @@ function App() {
                 <Route path={'/login'} element={<div>Login</div>}/>
                 <Route path={'/profile/:x/:y'} element={<Profile/>}/>
             </Routes>
+
+            <a
+                href={'https://github.com/adisovaa/react-router-dom-v6/tree/main/src'}
+                target={'_blank'}
+                rel={'noreferrer nofollow noopener'}
+            >target blank</a>
+
 
             {/*<Routes>*/}
             {/*    <Route path={'/'} element={<div>Main</div>}/>*/}
