16612de934e590d4e5cfbd83fa5a8d3585045755
lorem <79251934+adisovaa@users.noreply.github.com>
2023-04-30 13:00:47 -0400

    03 - useParams

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import './App.css';
-import {NavLink, Outlet, Route, Router, Routes} from "react-router-dom";
+import {NavLink, Outlet, Route, Router, Routes, useParams} from "react-router-dom";
+
+const Profile = () => {
+    const param = useParams<'x' | 'y'>()
+    const some = param.x
+    console.log(some)
+
+    return <div>Profile</div>
+}
 
 function App() {
     return (
@@ -8,25 +16,32 @@ function App() {
             <NavLink to={'/'}>Main</NavLink> ---
             <NavLink to={'/login'}>Login</NavLink> ---
             <NavLink to={'/profile'}>Profile</NavLink> ---
-            <NavLink to={'/profile/settings'}>Settings</NavLink> ---
+            <NavLink to={'/profile/1'}>Profile</NavLink> ---
 
 
             <Routes>
                 <Route path={'/*'} element={<div>404</div>}/>
                 <Route path={'/'} element={<div>Main</div>}/>
                 <Route path={'/login'} element={<div>Login</div>}/>
-                {/*<Route path={'/profile/*'} element={<div>Profile</div>}/>*/}
-                <Route path={'/profile'} element={(
-                    <div>
-                        Profile
-                        <Outlet/>
-                    </div>
-                )}/>
-                {/*<Route path={'*'} element={<div>profile page not found</div>}/>*/}
-                <Route index element={<div>check id</div>}/>
-                <Route path={':id'} element={<div>id</div>}/>
-                <Route path={'settings'} element={<div>Settings</div>}/>
+                <Route path={'/profile/:x'} element={<Profile/>}/>
             </Routes>
+
+            {/*<Routes>*/}
+            {/*    <Route path={'/'} element={<div>Main</div>}/>*/}
+            {/*    <Route path={'/login'} element={<div>Login</div>}/>*/}
+            {/*    <Route path={'/profile/*'} element={<div>Profile</div>}/>*/}
+            {/*    /!*<Route path={'/profile'} element={(*!/*/}
+            {/*    /!*    <div>*!/*/}
+            {/*    /!*        Profile*!/*/}
+            {/*    /!*        <Outlet/>*!/*/}
+            {/*    /!*    </div>*!/*/}
+            {/*    /!*)}/>*!/*/}
+            {/*    /!*<Route path={'*'} element={<div>profile page not found</div>}/>*!/*/}
+            {/*    /!*<Route index element={<div>check id</div>}/>*!/*/}
+            {/*    */}
+            {/*    <Route path={':id'} element={<div>id</div>}/>*/}
+            {/*    <Route path={'settings'} element={<Profile/>}/>*/}
+            {/*</Routes>*/}
         </div>
     );
 }
